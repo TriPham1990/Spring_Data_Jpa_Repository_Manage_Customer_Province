d22df7ce894058911b6906597a552e1196fed5c0
TriPham1990 <pmtri1990@gmail.com>
2019-10-01 17:28:52 +0700

    ok

---
+++ src/main/java/tri/lo/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package tri.lo.repository;
+
+import tri.lo.model.Customer;
+
+public interface CustomerRepository extends Repository<Customer> {
+}
---
+++ src/main/java/tri/lo/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,33 @@
+package tri.lo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tri.lo.model.Customer;
+import tri.lo.repository.CustomerRepository;
+import tri.lo.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.remove(id);
+    }
+}
