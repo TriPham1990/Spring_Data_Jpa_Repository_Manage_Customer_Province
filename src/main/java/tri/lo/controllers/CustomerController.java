ec9b2997e231d5693a9d1a750aeccc1281be5ef0
TriPham1990 <pmtri1990@gmail.com>
2019-10-01 17:59:48 +0700

    update

--- src/main/java/tri/lo/controllers/CustomerController.java
+++ src/main/java/tri/lo/controllers/CustomerController.java
@@ -1,13 +1,32 @@
 package tri.lo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import tri.lo.model.Customer;
+import tri.lo.service.CustomerService;
 
 @Controller
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
 
-    @GetMapping("/")
-    public String home(){
-        return "index";
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateCustomer(){
+        ModelAndView modelAndView = new ModelAndView("customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView createCustomer(@ModelAttribute Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "New customer created successfully");
+        return modelAndView;
     }
 }
