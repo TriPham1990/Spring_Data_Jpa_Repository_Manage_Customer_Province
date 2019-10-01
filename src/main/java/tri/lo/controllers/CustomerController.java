5f602b63220f39be6ecb8f8654d89cb614cf3261
TriPham1990 <pmtri1990@gmail.com>
2019-10-01 22:57:01 +0700

    complete customer

--- src/main/java/tri/lo/controllers/CustomerController.java
+++ src/main/java/tri/lo/controllers/CustomerController.java
@@ -2,12 +2,13 @@ package tri.lo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import tri.lo.model.Customer;
 import tri.lo.service.CustomerService;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
@@ -22,11 +23,49 @@ public class CustomerController {
     }
 
     @PostMapping("/create-customer")
-    public ModelAndView createCustomer(@ModelAttribute Customer customer){
+    public String createCustomer(@ModelAttribute Customer customer, RedirectAttributes redirect){
         customerService.save(customer);
-        ModelAndView modelAndView = new ModelAndView("customer/create");
-        modelAndView.addObject("customer", new Customer());
-        modelAndView.addObject("message", "New customer created successfully");
+        redirect.addFlashAttribute("message", "New customer created successfully");
+        return "redirect:/customers";
+    }
+
+    @GetMapping("/customers")
+    public ModelAndView showListCustomer(){
+        List<Customer> customers = customerService.findAll();
+        ModelAndView modelAndView = new ModelAndView("customer/index");
+        modelAndView.addObject("customers", customers);
         return modelAndView;
+    }
+
+    @GetMapping("/edit-customer/{id}")
+    public ModelAndView showEditCustomer(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("customer/edit");
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+
+    @PostMapping("/edit-customer")
+    public ModelAndView editCustomer(@ModelAttribute Customer customer){
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/edit");
+        modelAndView.addObject("customer", customer);
+        modelAndView.addObject("message", "Customer updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-customer/{id}")
+    public ModelAndView showDeleteCustomer(@PathVariable Long id){
+        Customer customer = customerService.findById(id);
+        ModelAndView modelAndView = new ModelAndView("customer/delete");
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+
+    @PostMapping("/delete-customer")
+    public String deleteCustomer(@ModelAttribute Customer customer, RedirectAttributes redirect){
+        customerService.remove(customer.getId());
+        redirect.addFlashAttribute("message", "Customer remove successfully");
+        return "redirect:/customers";
     }
 }
